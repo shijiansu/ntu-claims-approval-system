5d9b332f9c22bdf2e79c07baa9cbfe4fbfabe397
Shijian Su <sushijian.iss.nus@gmail.com>
2020-05-24 23:38:33 +0800

    Add project structure files

---
+++ setup-database.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+# 20200524
+# define variable
+application_name="ntu-claims-approval-system"
+container_name="${application_name}-mysql"
+database_name="ntu_claims_approval_system"
+database_password="root"
+
+# clean up mysql container
+docker container stop "${container_name}"
+docker container rm "${container_name}"
+
+# start mysql container
+docker container run --restart always -p 3306:3306 -d \
+  --name "${container_name}" \
+  -e MYSQL_DATABASE="${database_name}" \
+  -e MYSQL_ROOT_PASSWORD="${database_password}" \
+  mysql:8.0.20
+
+sleep 30s
+
+# access to mysql
+## docker container exec -it ntu-claims-approval-system-mysql /bin/bash
+## mysql -u root -p # or
+## mysql --user="root" --password="root" --database="ntu_claims_approval_system"
+
+# initiate database and data
+docker cp ./data/init.sql "${container_name}":/tmp/init.sql
+docker container exec -it "${container_name}" \
+  mysql --user="root" --password="${database_password}" --database="${database_name}" \
+  -e "source /tmp/init.sql" # source: load data file; -e: execute command in mysql
+
+# verify setup
+docker container exec -it "${container_name}" \
+  mysql --user="root" --password="${database_password}" --database="${database_name}" \
+  -e "show databases; use ${database_name}; show tables;"
